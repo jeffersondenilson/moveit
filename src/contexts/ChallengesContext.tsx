8b98c2f3ffc0fd81de8d44cc0e41507b60301096
jeffersondenilson <jefersond121212@gmail.com>
2021-02-25 15:04:46 -0300

    context

---
+++ src/contexts/ChallengesContext.tsx
@@ -0,0 +1,51 @@
+import { createContext, useState, ReactNode } from "react";
+import challenges from "../../challenges.json";
+
+interface Challenge {
+  type: "body" | "eye";
+  description: string;
+  amount: number;
+}
+
+interface ChallengesContextData {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+  activeChallenge: Challenge;
+  levelUp: () => void;
+  startNewChallenge: () => void;
+}
+
+interface ChallengesProviderProps {
+  children: ReactNode;
+}
+
+export const ChallengesContext = createContext({} as ChallengesContextData);
+
+export function ChallengesProvider( { children }: ChallengesProviderProps ) {
+  const [level, setLevel] = useState(1);
+  const [currentExperience, setCurrentExperience] = useState(0);
+  const [challengesCompleted, setChallengesCompleted] = useState(0);
+  const [activeChallenge, setActiveChallenge] = useState(null);
+
+  const levelUp = () => setLevel(level + 1);
+
+  const startNewChallenge = () => {
+    // const randomChallengeIndex = 
+  }
+
+  return (
+    <ChallengesContext.Provider
+      value={{
+        level,
+        currentExperience,
+        challengesCompleted,
+        levelUp,
+        startNewChallenge,
+        activeChallenge,
+      }}
+    >
+      {children}
+    </ChallengesContext.Provider>
+  );
+}
